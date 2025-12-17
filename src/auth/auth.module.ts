ada0a17169d81c4da108e8d4cd4e4835d4e10841
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-12-17 17:28:48 -0300

    module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,11 @@
-import { Module } from "@nestjs/common";
-import { AuthController } from "./auth.controller";
-import { AuthService } from "./auth.service";
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
-    imports: [],
-    controllers: [AuthController],
-    providers: [AuthService],
-    exports: []
+  imports: [],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [],
 })
-export class AuthModule{}
-
+export class AuthModule {}
