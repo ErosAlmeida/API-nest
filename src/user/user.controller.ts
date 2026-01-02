ff64683971fccbb75fef5ba03da703299a629179
ErosAlmeida <eros.almeida.vilela@gmail.com>
2026-01-01 22:21:35 -0300

    config nest

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,9 +3,11 @@ import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe'
 
 @Controller('user')
 export class UserController {
+  configService: any;
   @Get(':id')
   findOne(@Param('id', CustomParseIntPipe) id: number) {
-    console.log(id, typeof id);
+    console.log(process.env.TESTE1 || 'Padrão');
+    console.log(this.configService.getOrThrow('TESTE1'));
     return `olá do controller do user #${id}`;
   }
 }
